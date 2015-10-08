a77afbfb90712e6cdcf9c6734a2b5e92ab4ce400
ntoscano <Nicolas.Toscano@gmail.com>
2015-10-07 20:14:07 -0700

    implemented basic hidding functionality. TODO: remove text when hidden

--- app/controllers/deckController.js
+++ app/controllers/deckController.js
@@ -11,13 +11,13 @@
     var _addSelector = function(CID, question){
       if(question){
         if(selectedQuestionID){
-          $scope.deck.cards[selectedQuestionID].state = 'hidden';
+          $scope.deck.cards[selectedQuestionID].state = 'covered';
         }
         $scope.deck.cards[CID].state = 'qSelected';
         selectedQuestionID = CID;        
       }else{
         if(selectedAnswerID){
-          $scope.deck.cards[selectedAnswerID].state = 'hidden';
+          $scope.deck.cards[selectedAnswerID].state = 'covered';
         }
         $scope.deck.cards[CID].state = 'aSelected';
         selectedAnswerID = CID;
@@ -26,10 +26,10 @@
 
     var _removeSelector = function(){
       if(selectedQuestionID){
-        $scope.deck.cards[selectedQuestionID].state = 'hidden';
+        $scope.deck.cards[selectedQuestionID].state = 'covered';
       }
       if(selectedAnswerID){
-        $scope.deck.cards[selectedAnswerID].state = 'hidden';
+        $scope.deck.cards[selectedAnswerID].state = 'covered';
       }
     }
 
