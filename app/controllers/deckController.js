360b123780ac2ba4b06e013a6330b6ccb098c70f
ntoscano <Nicolas.Toscano@gmail.com>
2015-10-07 18:58:18 -0700

    implemented state functionality

--- app/controllers/deckController.js
+++ app/controllers/deckController.js
@@ -4,22 +4,43 @@
 
   app.controller('deckCtrl', ['$scope','deckSrvc', function($scope, deckService){
     $scope.deck = deckService.getDeck();
-    $scope.select = function(CID, question){
+    $scope.matching = false;
+    var selectedAnswerID = null;
+    var selectedQuestionID = null;
+
+    var _addSelector = function(CID, question){
       if(question){
-        for(var i in $scope.deck.cards){
-          $scope.deck.cards[i].qSelected = 0;
-        }
-        $scope.deck.cards[CID].qSelected = 1;
+        if(!selectedQuestionID) selectedQuestionID = CID;
+        $scope.deck.cards[selectedQuestionID].qSelected = false;
+        $scope.deck.cards[CID].qSelected = true;
+        selectedQuestionID = CID;        
       }else{
-        for(var i in $scope.deck.cards){
-          $scope.deck.cards[i].aSelected = 0;
+        if(!selectedAnswerID) selectedAnswerID = CID;
+        $scope.deck.cards[selectedAnswerID].aSelected = false;
+        $scope.deck.cards[CID].aSelected = true;
+        selectedAnswerID = CID;
+      } 
+    }
+
+    var _removeSelector = function(){
+      $scope.deck.cards[selectedQuestionID].qSelected = false;
+      $scope.deck.cards[selectedAnswerID].aSelected = false;
+    }
+
+    $scope.select = function(CID, question){
+      if(!$scope.deck.cards[CID].matched){
+        if($scope.matching){
+          _addSelector(CID, question);
+
+          if(selectedQuestionID === selectedAnswerID) $scope.deck.cards[CID].matched = true;
+
+          _removeSelector();
+          $scope.matching = false;
+        }else{
+          _addSelector(CID, question);
+          $scope.matching = true;
         }
-        $scope.deck.cards[CID].aSelected = 1;
       }
-
-      if($scope.deck.cards[CID].aSelected && $scope.deck.cards[CID].qSelected) console.log('match');
-      else console.log('nonmatch');
-
     }
 
 
